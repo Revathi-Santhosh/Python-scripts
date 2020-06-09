dbe7fd8575dc780529cbdc979a1116131051e0ce
santhosh ruttala <revathisanthosh.lalam@gmail.com>
2020-06-09 15:56:57 +0100

    Modified findastring file

--- Findastring.py
+++ Findastring.py
@@ -38,8 +38,19 @@
 #
 
 def count_substring(string, sub_string):
-    sub_string_count = string.count(sub_string)
-    return sub_string_count
+    count=0
+    #print(len(string),len(sub_string))
+    for i in range(0, len(string)-len(sub_string)+1):
+        if string[i] == sub_string[0]:
+            flag=1
+            i =0
+            for j in range (0, len(sub_string)):
+                if string[i+j] != sub_string[j]:
+                    flag=0
+                    break
+            if flag==1:
+                count += 1
+    return count
 
 if __name__ == '__main__':
     string = input().strip()
